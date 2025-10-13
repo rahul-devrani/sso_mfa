450659b8406f5b8fab59e33e16339efdf2e50829
Rahul Devrani <142100974+rahul-devrani@users.noreply.github.com>
2025-10-13 19:34:01 +0530

    Add files via upload

---
+++ client_app_1/app1.py
@@ -0,0 +1,25 @@
+import os, sys
+from flask import Flask, render_template, redirect, url_for
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from shared.config import Config
+from shared.auth_utils import get_user_from_cookie, require_auth
+
+app = Flask(__name__, template_folder='.')
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    user = get_user_from_cookie()
+    # Agar user logged in hai toh 'Hello' dikhao, nahi toh 'Login' button
+    return render_template('app_template.html', app_name="App 1", user=user)
+
+@app.route('/login')
+@require_auth  # Is route ka kaam sirf SSO redirect trigger karna hai
+def login():
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    print("🚀 Client App 1 chalu ho raha hai http://localhost:5001 par")
+    app.run(debug=True, port=5001, host='localhost')
