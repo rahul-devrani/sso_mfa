2e4fe30cdeeb4f70ee35516092d29e3b8b17364a
Rahul Devrani <142100974+rahul-devrani@users.noreply.github.com>
2025-11-19 21:36:57 +0530

    Update config.py

--- shared/config.py
+++ shared/config.py
@@ -1,6 +1,10 @@
+import os
+basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 class Config:
-    SECRET_KEY = 'a-super-secret-key'
-    JWT_SECRET_KEY = 'a-different-jwt-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sso.db'
+    
+    SECRET_KEY = 'secret-key'
+    JWT_SECRET_KEY = 'jwt-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'sso.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     AUTH_SERVER_URL = 'http://localhost:5000'
