4e6d613b3ae67728651cb6968a1a995d2ea6b2bb
Rahul Devrani <142100974+rahul-devrani@users.noreply.github.com>
2025-11-19 21:32:34 +0530

    Update models.py

--- auth_server/models.py
+++ auth_server/models.py
@@ -7,10 +7,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(256))
-    mfa_secret = db.Column(db.String(128), nullable=False) # Har user ka MFA secret hoga
+
+    mfa_secret = db.Column(db.String(128), nullable=True) 
+    mfa_enabled = db.Column(db.Boolean, nullable=False, default=True) 
+    
+    role = db.Column(db.String(20), nullable=False, default='user') 
     
     def set_password(self, password):
+       
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
+    
         return check_password_hash(self.password_hash, password)
